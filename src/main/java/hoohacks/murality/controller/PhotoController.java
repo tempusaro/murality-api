52cec62fa6250a21b230e8e9cc0e19b74ab0bfbd
Baiyi Zhang <100baiyi@gmail.com>
2024-03-24 02:54:40 -0400

    storage: init

--- src/main/java/hoohacks/murality/controller/PhotoController.java
+++ src/main/java/hoohacks/murality/controller/PhotoController.java
@@ -1,18 +1,40 @@
 package hoohacks.murality.controller;
+import hoohacks.murality.service.StorageService;
 
 import hoohacks.murality.module.Response;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/photo")
 public class PhotoController {
 
+    @Autowired
+    private StorageService service;
 
-    @PutMapping("upload")
-    public Response uploadPicture() {
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam(value = "file") MultipartFile file) {
+        return new ResponseEntity<>(service.uploadFile(file), HttpStatus.OK);
+    }
 
-        return new Response();
+    @GetMapping("/download/{pid}")
+    public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable String pid) {
+        byte[] data = service.downloadFile(pid);
+        ByteArrayResource resource = new ByteArrayResource(data);
+        return ResponseEntity
+                .ok()
+                .contentLength(data.length)
+                .header("Content-type", "application/octet-stream")
+                .header("Content-disposition", "attachment; filename=\"" + pid + "\"")
+                .body(resource);
+    }
+
+    @DeleteMapping("/delete/{pid}")
+    public ResponseEntity<String> deleteFile(@PathVariable String pid) {
+        return new ResponseEntity<>(service.deleteFile(pid), HttpStatus.OK);
     }
 }
